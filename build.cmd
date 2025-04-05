cc819d07ed2bad967b3aee3d3e21075d495c23c8
Arthur Roos <artur.roos@ktnlvr.dev>
2025-04-06 02:30:33 +0300

    Add RTC synchronization to the build script

--- build.cmd
+++ build.cmd
@@ -1,4 +1,5 @@
 git submodule update --recursive --remote
 python -m mpremote cp -r src/. :
 python -m mpremote cp -r external/metropolia-pico-lib :lib
+python -m mpremote rtc --set
 python -m mpremote run ./src/main.py
