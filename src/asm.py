dbfb6cd4c9f0f0d12f9fc6d9ea9c96a53b9cb1de
Kartik Patel <166979981+KartikPat250905@users.noreply.github.com>
2025-04-05 17:53:47 +0300

    Added heart rate prints

--- src/asm.py
+++ src/asm.py
@@ -10,7 +10,10 @@ from constants import (
     DISPLAY_WIDTH,
     MEAN_WINDOW_PERCENT,
     MEAN_WINDOW_SIZE,
+    MIN_PEAK_INTERVAL,
+    MAX_PEAK_INTERVAL,
 )
+import time
 
 
 class Machine(HAL):
@@ -28,6 +31,8 @@ class Machine(HAL):
             ],
         )
         self.samples = []
+        self.last_peak = None
+        self.peak_diffs = []
 
     def main_menu(self):
         self.main_menu_ui.tick()
@@ -40,20 +45,51 @@ class Machine(HAL):
             (value - min_value) / (max_value - min_value) * (DISPLAY_HEIGHT - 1)
         )
 
+    #samples stores the values to show on screen
+    #mean_window stores the smoothed out values
+    #peak_diffs would store the difference between the peaks
     def heart_rate(self):
-        prev_x = 0
         prev_y = DISPLAY_HEIGHT - 1
         samples_on_screen = []
-        corrected_mean = 0
+        mean_window = []
         while True:
             self.display.fill(0)
+            mean = sum(mean_window) / len(mean_window) if len(mean_window) else 0
+            corrected_mean = 0
+            if mean != 0:
+                min_window = min(mean_window)
+                max_window = max(mean_window)
+                corrected_mean = MEAN_WINDOW_PERCENT * (mean - min_window) + min_window
             new_samples = []
-            for i in range(SAMPLES_PER_PIXEL):
-                new_sample = self.sensor_pin_adc.read_u16()
-                new_samples.append(new_sample)
-            self.samples.append(sum(new_samples) / len(new_samples))
+            new_samples = [self.sensor_pin_adc.read_u16() for _ in range(SAMPLES_PER_PIXEL)]
+            filtered_sample = sum(new_samples) / len(new_samples)
+            self.samples.append(filtered_sample)
             if len(self.samples) > SAMPLE_SIZE:
                 self.samples = self.samples[-SAMPLE_SIZE:]
+            if self.samples:
+                filtered_sample = self.low_pass_filter(self.samples[-1], filtered_sample)
+            mean_window.append(filtered_sample)
+            if len(mean_window) > SAMPLE_SIZE:
+                mean_window = mean_window[-SAMPLE_SIZE:]
+            current_time = time.ticks_ms()
+            if len(self.samples) > 2:
+                next = self.samples[-1]
+                current = self.samples[-2]
+                prev = self.samples[-3]
+                if current > next and current > prev and current > corrected_mean:
+                    if self.last_peak is not None:
+                        peak_dif = time.ticks_diff(current_time, self.last_peak)
+                        if peak_dif >= MIN_PEAK_INTERVAL and peak_dif <= MAX_PEAK_INTERVAL:
+                            print("PEAK DETECTED")
+                            print(f"The current heart rate is {60000 / mean_peak if mean_peak != 0 else 0}")
+                            self.peak_diffs.append(peak_dif)
+                            if len(self.peak_diffs) > SAMPLE_SIZE:
+                                self.peak_diffs = self.peak_diffs[-SAMPLE_SIZE:]
+                    self.last_peak = current_time
+            if time.ticks_diff(current_time, self.last_peak) > 5000:
+                self.peak_diffs = []
+                heart_rate = 0
+
             samples_on_screen = (
                 self.samples[-DISPLAY_WIDTH:]
                 if len(self.samples) > DISPLAY_WIDTH
@@ -63,49 +99,18 @@ class Machine(HAL):
             min_value = min(samples_on_screen)
             if max_value == min_value:
                 max_value += 1
+            prev_x = 0
             for screen_x in range(len(samples_on_screen)):
                 screen_y = self.min_max_scaling(
                     max_value, min_value, samples_on_screen[screen_x]
                 )
                 self.display.pixel(screen_x, screen_y, 1)
                 self.display.line(prev_x, prev_y, screen_x, screen_y, 1)
-                self.display.show()
-                prev_x = screen_x
-                prev_y = screen_y
-            prev_x = 0
+                prev_x, prev_y = screen_x, screen_y
+            self.display.show()
             if len(self.samples) >= 2:
-                prev_y = self.min_max_scaling(
-                    max_value, min_value, samples_on_screen[1]
-                )
-            peaks_t = []
-            mean_window = []
-            for i in range(1, len(self.samples)):
-                self.samples[i] = self.low_pass_filter(
-                    self.samples[i - 1], self.samples[i]
-                )
-            for i in range(1, len(self.samples) - 1):
-                mean = (sum(mean_window) / len(mean_window)) if len(mean_window) else 0
-                if mean != 0:
-                    mi = min(mean_window)
-                    ma = max(mean_window)
-                    corrected_mean = MEAN_WINDOW_PERCENT * (mean - mi) + mi
-                if (
-                    self.samples[i - 1] <= self.samples[i]
-                    and self.samples[i + 1] < self.samples[i]
-                    and self.samples[i] > corrected_mean
-                ):
-                    peaks_t.append((i + 1) * 0.004)
-                mean_window.append(
-                    self.low_pass_filter(self.samples[i], corrected_mean)
-                )
-                if len(mean_window) >= MEAN_WINDOW_SIZE:
-                    mean_window.pop(0)
-            if len(peaks_t) > 1:
-                for i in range(0, len(peaks_t) - 1):
-                    peaks_t[i] = peaks_t[i + 1] - peaks_t[i]
-                peaks_t = peaks_t[:-1]
-                mean_peaks = sum(peaks_t) / len(peaks_t)
-                print(f"The current heart rate is {60 / mean_peaks} and {peaks_t}")
+                prev_y = self.min_max_scaling(max_value, min_value, samples_on_screen[1])
+            mean_peak = sum(self.peak_diffs) / len(self.peak_diffs) if len(self.peak_diffs) != 0 else 0
 
     def toast(self):
         if self.button():
--- src/constants.py
+++ src/constants.py
@@ -47,7 +47,7 @@ TIMESTAMP_DIFFERENCE_SENSOR = 0.004
 # Alpha value for the low pass filter
 ALPHA = 0.5
 # Samples per pixel
-SAMPLES_PER_PIXEL = 5
+SAMPLES_PER_PIXEL = 3
 # Pin number of the heart beat sensor
 PIN_SENSOR = 27
 # Total samples to take in account when calculating heart beat measurements
@@ -57,3 +57,5 @@ SAMPLE_SIZE = 200
 MEAN_WINDOW_SIZE = 50
 # Scaling factor for the next peak
 MEAN_WINDOW_PERCENT = 1.5
+MIN_PEAK_INTERVAL = 600
+MAX_PEAK_INTERVAL = 1000
