97dc25772d55f53c3d30732b82fe62d74a6fe61d
Arthur Roos <artur.roos@ktnlvr.dev>
2025-04-17 00:10:21 +0300

    Check for the correct firmware version

--- src/main.py
+++ src/main.py
@@ -1,3 +1,16 @@
+from sys import implementation, exit
+
+major, minor, *_ = implementation.version
+
+if major < 1 or major == 1 and minor < 25:
+    print(
+        f"Hey! You are running MicroPython {major}.{minor} "
+        "but the minimum supported version is 1.25, "
+        "upgrade your MicroPython and come back!"
+    )
+    exit()
+
+
 from asm import Machine
 from logging import log
 from wifi import connect_ap
