844d0c2c4d1afe01958ba8606086606212c6b6eb
Arthur Roos <artur.roos@ktnlvr.dev>
2025-04-26 15:42:20 +0300

    Dispay datetime with proper padding

--- src/utils.py
+++ src/utils.py
@@ -3,4 +3,4 @@ from time import localtime
 
 def localtime_string() -> str:
     Y, M, D, H, m, s, *_ = localtime()
-    return f"{Y}-{M}-{D}T{H}:{m}:{s}"
+    return f"{Y:04}-{M:02}-{D:02}T{H:02}:{m:02}:{s:02}"
